5a38d071c860dbee7a06309d249cfde7419ad7c9
sukhsung@umich.edu <Tjrgus93>
2019-10-25 14:03:59 -0400

    removing all

--- test.cpp
+++
@@ -1,6 +0,0 @@
-#define WASM_EXPORT __attribute__((visibility("default")))
-
-WASM_EXPORT
-int add(int num1, int num2){
-    return num1+num2;
-}
--- wa.js
+++
@@ -1,7 +0,0 @@
-
-(async () => {
-    const fetchPromise = fetch('test.wasm');
-    const { instance } = await WebAssembly.instantiateStreaming(fetchPromise);
-    const result = instance.exports.add(40,2);
-    console.log(result);
-  })();
