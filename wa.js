854a3deedaca1fdd9a9613a8f2296a6dadf1c769
sukhsung@umich.edu <Tjrgus93>
2019-10-25 14:27:35 -0400

    basic image displayer

---
+++ wa.js
@@ -0,0 +1,44 @@
+function drawGrayscaleBitmap(ctx,bitmap) {
+    for(var it = 0; it < numPx; it++)
+    {
+        for(var jt = 0; jt < numPx; jt++)
+        {
+            var value = bitmap[it][jt];
+            var part = Number(parseInt( value , 10)).toString(16);
+            if(part.length <2 )
+            {
+                part = "0"+part;
+            }
+            var color = '#' + part+part+part;
+            ctx.fillStyle=color;
+            ctx.fillRect(it,jt,1,1);
+        }
+    }
+}
+
+function makeTestImage(numPx) {
+  var image = math.zeros(numPx,numPx);
+  for(var it = 0; it < numPx; it++) {
+    for(var jt = 0; jt < numPx; jt++) {
+      image._data[it][jt] = it+jt
+    }
+  }
+  return image
+}
+
+var numPx = 512
+var canvas = document.getElementById("canvas1");
+var ctx = canvas.getContext("2d");
+canvas.width = numPx;
+canvas.height = numPx;
+
+var image = makeTestImage(numPx)
+
+drawGrayscaleBitmap(ctx,image._data);
+
+// WebAssembly.instantiateStreaming(fetch('../out/main.wasm'))
+// .then(obj => {
+//   // Call an exported function:
+//   console.log(obj.instance.exports.add(40,2));
+// })
+
